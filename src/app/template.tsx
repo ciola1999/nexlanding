087aea8b2105154a27b5891403cbc0a9ae9965fe
Mirhop <ciolaarjunior31@gmail.com>
2025-12-26 09:59:42 +0700

    Akhirnya selesai fase 2

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,16 +1,25 @@
 // app/template.tsx
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  
+  // Kita pakai jurus 'as any' lagi biar TypeScript tidak rewel
+  const MotionDiv = (motion as any).div;
+
   return (
-    <motion.div
+    <MotionDiv
+      // Posisi Awal: Sedikit di bawah (y: 20) dan transparan (opacity: 0)
       initial={{ y: 20, opacity: 0 }}
+      
+      // Posisi Akhir: Kembali ke posisi normal (y: 0) dan jelas (opacity: 1)
       animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: 'easeInOut', duration: 0.75 }}
+      
+      // Durasi: 0.75 detik (agak lambat biar elegan/cinematic)
+      transition={{ ease: "easeInOut", duration: 0.75 }}
     >
       {children}
-    </motion.div>
+    </MotionDiv>
   );
 }
